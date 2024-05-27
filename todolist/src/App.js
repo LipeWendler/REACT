1644ed75cc5ca1bd47f0a8de0d8aa3c80e69ff59
LipeWendler <felipewendlerccbeu@gmail.com>
2024-05-27 19:50:04 -0300

    att

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,38 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
+import "./App.css"
 
-function App(){
+function App() {
   const [tarefas, setTarefas] = useState([]);
 
   const [input, setInput] = useState('');
 
-  useEffect(() =>{
+  const [numero, setNumero] = useState(0);
+
+  const calculo = useMemo(() => {
+    let resultado = 0;
+    for (let i = 0; i < numero * 1000; i++) {
+      resultado += i;
+    }
+    return resultado;
+  }, [numero]);
+
+  useEffect(() => {
     const tarefasStorage = localStorage.getItem('tarefas');
 
-    if(tarefasStorage){
+    if (tarefasStorage) {
       setTarefas(JSON.parse(tarefasStorage));
     }
-  },[]);
+  }, []);
 
   useEffect(() => {
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
-  },[tarefas]);
+  }, [tarefas]);
 
-  function adicionarTarefa(){
+  function adicionarTarefa() {
     setTarefas([...tarefas, input]);
     setInput('');
   }
-return(
-  <div>
-    <ul className='lista'>
-      {tarefas.map(tarefa => (
-      <li key={tarefa}> {tarefa} </li>
-      ))}
-    </ul>
+  return (
+    <>
+      <div>
+        <ul className='lista'>
+          {tarefas.map(tarefa => (
+            <li key={tarefa}> {tarefa} </li>
+          ))}
+        </ul>
 
-    <input className='texto' type='text' value={input} onChange={e => setInput(e.target.value)}/>
-    <button  className='botao' type='button' onClick={adicionarTarefa}>Adicionar</button>
-  </div>
-)
+        <input className='texto' type='text' value={input} onChange={e => setInput(e.target.value)} />
+        <button className='botao' type='button' onClick={adicionarTarefa}>Adicionar</button>
+      </div>
+      <div>
+        <p>Número: {numero}</p>
+        <p>Resultado do cálculo: {calculo}</p>
+        <button className='botao' onClick={()=> setNumero(numero+1)}>Incrementar</button>
+        <button className='botao-reset' onClick={()=> setNumero(0)}>Reset</button>
+      </div>
+    </>
+  )
 }
 
 export default App;
