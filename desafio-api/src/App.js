e6ff5e2ad3997f5a705770e9a287d3cc79997e78
LipeWendler <felipewendlerccbeu@gmail.com>
2024-08-13 22:12:31 -0300

    Final

--- desafio-api/src/App.js
+++ desafio-api/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import './App.css';
+import React, { useState, useEffect } from "react"; //Import do React e Hooks
+import './App.css'; //Vinculação com o CSS
 
 
 export default function App() {
-  const [api, setApi] = useState([]);
+  const [api, setApi] = useState([]); 
 
   useEffect(() => {
     let url = 'https://financialmodelingprep.com/api/v3/search-name?query=META&limit=10&exchange=NASDAQ&apikey=O0sThOVzIeU7AlYn3pi3w7kSxYxALMfN'; //Variável com a URL do site para consumo
@@ -14,20 +14,22 @@ export default function App() {
 
   return (
     <>
+    {/* DEFINIÇÃO DO HEADER ================================== */}
       <header className="header-Container">
-        <strong className="header-Title">Companias da Bolsa de Volores - <span>EUROPA</span></strong>
+        <strong className="header-Title">Bolsa de Volores - <span>ESTADOS UNIDOS</span></strong>
       </header>
-
+    
       <div className="main-Container">
 
+        {/* Pega info da API e retorna através os valores encontrados por key/value  ================== */}
         {api.map((item) => {
           return (
             <>
               <article className="stock-Card">
                 <strong className="stock-Symbol">{item.symbol}</strong>
-                <p className="stock-Name white"><strong>Nome</strong>: {item.name}</p>
-                <p className="stock-Currency white"><strong>Moeda</strong>: {item.currency}</p>
-                <p className="stock-Exchange white"><strong>Bolsa de Valores</strong>: {item.stockExchange}</p>
+                <p className="stock-Desc white"><strong>Nome</strong>: {item.name}</p>
+                <p className="stock-Desc white"><strong>Moeda</strong>: {item.currency}</p>
+                <p className="stock-Desc white"><strong>Bolsa de Valores</strong>: {item.stockExchange}</p>
               </article>
             </>
           );
